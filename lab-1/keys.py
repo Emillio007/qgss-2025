dae1318f9a9199d8b173c2842e9cb3932ddc5b34
Emil Henningsen <emilhenningsen3@gmail.com>
2025-07-17 15:02:07 +0200

    completed exercises < 2.3

---
+++ lab-1/keys.py
@@ -0,0 +1,5 @@
+def api():
+	return "Eo5UPsX9PYTCANoANbK1B8uPWUjNspSFGBTgtvTp9U9Y"
+
+def crn():
+	return "crn:v1:bluemix:public:quantum-computing:us-east:a/dfbcb96e2e754363a83c1679549c5193:0c18cb2a-30c1-4d2c-baf2-b7cc9da995e9::"
